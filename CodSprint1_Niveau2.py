1e91beafbe13d471137245ecbe18b89f8ee48bef
SleoNiiX <cardosomoreira.anthony@gmail.com>
2023-09-30 17:44:25 +0200

    coding sprint 1 : 1.2 Choix d’un niveau de difficulte

---
+++ CodSprint1_Niveau2.py
@@ -0,0 +1,148 @@
+"""
+Created on Sat Sep 30 14:59:18 2023
+
+@author: Cardoso
+"""
+import random as r
+
+
+
+def Calcul_mental_addition():
+    """
+        Propose l’exercice de calcul mental
+    consistant en l’addition de ces deux nombres 
+    tirer aleatoirement grace au module random.
+    """
+    #On affecte deux nombres aleatoires aux variables nb_1 et nb_2, Et on calcule la somme de ces nombres
+    nb_1, nb_2 = r.randint(10, 99), r.randint(10, 99)   
+    somme = nb_1 + nb_2
+    
+    #On demande à l'utilisateur de calculer cette somme et on lui affiche si il a raison ou non
+    réponse = int(input(f"Quel est le resultat de l'operation {nb_1} + {nb_2} : "))
+    print(f"Bravo ! la reponse etait bien {réponse}" if somme == réponse else f"Oh non... Tu t'es trompe, la reponse etait : {somme}")
+
+
+def Boucle_calc_mental_addition():
+    """
+        Demande a l’utilisateur combien
+    d’additions il souhaite resoudre dans l’exercice.
+    Il lui sera alors propose autant d'addition.
+    """
+    #On demande combien d'addition il souhaite faire et fais une boucle de n calcul souhaitez en reprenant la fonction precedente
+    nombre_calcul = int(input("Combien d'addition veux-tu faire ? "))
+    
+    for i in range(nombre_calcul):
+        #On affecte deux nombres aleatoires aux variables nb_1 et nb_2, Et on calcule la somme de ces nombres
+        nb_1, nb_2 = r.randint(10, 99), r.randint(10, 99)   
+        somme = nb_1 + nb_2
+        
+        #On demande à l'utilisateur de calculer cette somme et on lui affiche si il a raison ou non
+        réponse = int(input(f"\nOperation {i+1} sur {nombre_calcul} : {nb_1} + {nb_2} = "))
+        print(f"Bravo ! la reponse etait bien {réponse}" if somme == réponse else f"Oh non... Tu t'es trompe, la reponse etait : {somme}")
+    
+    print("\nC'est fini ! tu as fais toutes les additions !")
+
+
+def Boucle_calc_mental_addition2():
+    """
+        Version de la fonction precedente
+    avec un compteur de bonne reponse.
+    """
+    #On initialise un compteur de bonne réponse
+    compteur = 0
+    nombre_calcul = int(input("Combien d'addition veux-tu faire ? "))
+    
+    for i in range(nombre_calcul):
+        nb_1, nb_2 = r.randint(10, 99), r.randint(10, 99)   
+        somme = nb_1 + nb_2
+        
+        réponse = int(input(f"\nOperation {i+1} sur {nombre_calcul} : {nb_1} + {nb_2} = "))
+        
+        #On incremente de 1 le compteur à chaque bonne réponse
+        if somme == réponse:
+            print(f"Bravo ! la reponse etait bien {réponse}")
+            compteur += 1
+        else:
+            print(f"Oh non... Tu t'es trompe, la reponse etait : {somme}")
+    
+    #Calcul du pourcentage de bonne reponse arrondi a deux chiffres apres la virgule avec round()
+    pourcentage_reponse = round(compteur * 100 / nombre_calcul,2)
+    
+    #Affiche une phrase personnalise selon son taux de bonne reponse
+    if pourcentage_reponse == 100:
+        print("Parfait ! 100% de bonne reponse !")
+    elif pourcentage_reponse < 30:
+        print(f"Retourne a l’ecole ! Tu n'as fais que {pourcentage_reponse}% de bonne reponse")
+    elif pourcentage_reponse < 60: 
+        print(f"Des progres sont necessaires. Ton pourcentage de bonne reponse est de {pourcentage_reponse}%")
+    elif pourcentage_reponse < 90:
+        print(f"Bons resultats. Tu as reussi {pourcentage_reponse}% des calculs.")
+    else:
+        print(f"Calculateur hors pair ! {pourcentage_reponse}% de tes calculs sont juste !")
+    
+    
+ 
+### 1.1 Commencons simple ###    
+
+#Calcul_mental_addition()
+#Boucle_calc_mental_addition()
+#Boucle_calc_mental_addition2()
+
+### Decommenter pour tester les fonctions ! ###
+
+
+
+def Difficulte_bcma():
+    """
+        bcma : Boucle_calc_mental_additio
+        
+        Demander d’entrer un entier d compris entre 0 et 4 a l'utilisateur.
+    S’il saisit d entre 1 et 4, vous lui demander des additions de nombres de
+    d chiffres exactement.
+    """
+    #Boucle  while qui demande le choix de la difficulté entre 0 et 4
+    choix_invalide = True
+    while choix_invalide:
+        choix_difficulte = int(input("De quel difficulte souhaites-tu que tes calculs soit entre 0 et 4 : "))
+        
+        if choix_difficulte >=0 and choix_difficulte <5: choix_invalide = False
+        
+    #Liste utiliser dans le cas ou choix_difficulte = 0
+    difficulte0 = [10, 100, 1000, 10000]
+    compteur = 0
+    nombre_calcul = int(input("Combien d'addition veux-tu faire ? "))
+    
+    for i in range(nombre_calcul):
+        #Liste qui defini le nombre de chiffre - 1. Et qui dans le cas de 0 vas donner un nombre de chiffre different a chaque calcul
+        difficulte = [difficulte0[r.randint(0,3)], 10, 100, 1000, 10000]
+        nb_1, nb_2 = r.randint(1, difficulte[choix_difficulte]-1), r.randint(1, difficulte[choix_difficulte]-1)   
+        somme = nb_1 + nb_2
+        
+        réponse = int(input(f"\nOperation {i+1} sur {nombre_calcul} : {nb_1} + {nb_2} = "))
+        
+        if somme == réponse:
+            print(f"Bravo ! la reponse etait bien {réponse}")
+            compteur += 1
+        else:
+            print(f"Oh non... Tu t'es trompe, la reponse etait : {somme}")
+    
+    pourcentage_reponse = round(compteur * 100 / nombre_calcul,2)
+    
+    if pourcentage_reponse == 100:
+        print("Parfait ! 100% de bonne reponse !")
+    elif pourcentage_reponse < 30:
+        print(f"Retourne a l’ecole ! Tu n'as fais que {pourcentage_reponse}% de bonne reponse")
+    elif pourcentage_reponse < 60: 
+        print(f"Des progres sont necessaires. Ton pourcentage de bonne reponse est de {pourcentage_reponse}%")
+    elif pourcentage_reponse < 90:
+        print(f"Bons resultats. Tu as reussi {pourcentage_reponse}% des calculs.")
+    else:
+        print(f"Calculateur hors pair ! {pourcentage_reponse}% de tes calculs sont juste !")
+        
+
+
+### 1.2 Choix d’un niveau de difficulte ###
+
+#Difficulte_bcma()
+
+### Decommenter pour tester les fonctions ! ###
